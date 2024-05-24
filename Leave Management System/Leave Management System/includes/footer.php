5a6725f3da3eed37af5e86b90e1e86826f929be7
SriHari211 <127759409+SriHari211@users.noreply.github.com>
2024-05-24 21:08:38 +0530

    Add files via upload

---
+++ Leave Management System/Leave Management System/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By <a href="https://codeastro.com">CodeAstro</a></p>
+        </div>
+</footer>
